77a6a4981bb47b820061bc2a6e62938c91dd3409
Seweryn Michota <sewerynmichota@gmail.com>
2024-06-03 19:52:11 +0100

    Change random texts on Index Page

--- views/index/index.php
+++ views/index/index.php
@@ -2,33 +2,32 @@
     <div class=" col-xl-9 col-lg-9 ">
         <h3>Welcome to AcademiChain</h3>
         <p>
-            A place where you can ask questions, provide answers and earn tokens!
+            A place where you can ask questions, provide answers and earn cryptocurrency tokens!
         </p>
 
         <p>
             <img class="img-fluid" src="public/img/struggling-student.webp" width="100%" alt="Student struggling with study materials" />
         </p>
 
-        <h2>How to get Tokens</h2>
+        <h2>How it works ?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi, delectus deserunt doloremque ea, esse eum eveniet facilis hic ipsam laboriosam maxime minima mollitia nobis nulla placeat repudiandae unde voluptatem!</p>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur eos est iusto labore nulla odit quaerat! Accusamus, adipisci aliquam beatae commodi dignissimos error id impedit libero omnis veniam voluptas?</p>
 
 
-        <h2>How to get Tokens</h2>
+        <h2>How to earn Tokens ?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi, delectus deserunt doloremque ea, esse eum eveniet facilis hic ipsam laboriosam maxime minima mollitia nobis nulla placeat repudiandae unde voluptatem!</p>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur eos est iusto labore nulla odit quaerat! Accusamus, adipisci aliquam beatae commodi dignissimos error id impedit libero omnis veniam voluptas?</p>
 
 
-        <h2>How to get Tokens</h2>
+        <h2>Can I use tokens outside AcademiChain ?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi, delectus deserunt doloremque ea, esse eum eveniet facilis hic ipsam laboriosam maxime minima mollitia nobis nulla placeat repudiandae unde voluptatem!</p>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur eos est iusto labore nulla odit quaerat! Accusamus, adipisci aliquam beatae commodi dignissimos error id impedit libero omnis veniam voluptas?</p>
 
 
-
-        <h2>How to get Tokens</h2>
+        <h2>What is cryptocurrency and tokens ?</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi, delectus deserunt doloremque ea, esse eum eveniet facilis hic ipsam laboriosam maxime minima mollitia nobis nulla placeat repudiandae unde voluptatem!</p>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur eos est iusto labore nulla odit quaerat! Accusamus, adipisci aliquam beatae commodi dignissimos error id impedit libero omnis veniam voluptas?</p>

816634906ea9b5de3080d1562d9af0d1875d370c
Seweryn <smicho01@student.bbk.ac.uk>
2024-07-04 20:28:53 +0100

    Add icons and check if best answer has been selected.

--- views/questions/modules/answer.php
+++ views/questions/modules/answer.php
@@ -22,7 +22,25 @@ if(isset($_SESSION['user'])){
         by: <span class="user-name"><?php echo $answer['userName']; ?></span>
     </div>
     <div class="card-body">
-        <?php echo nl2br($answer['content']); ?>
+        <div class="row">
+            <div class="col">
+                <?php echo nl2br($answer['content']); ?>
+                <?php print_r($answer); ?>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="row">
+            <div class="col">
+                <div class="row answer-like-bar">
+                    <div class="col-1"><span class="answer-like"><i class="ikonka hoverable hoverable-green fa-solid fa-thumbs-up"></i></span></div>
+                    <div class="col-2"><span class="answer-like-count">votes: 20</i></span></div>
+                    <div class="col-1"><span class="answer-unlike"><i class="ikonka hoverable hoverable-red fa-solid fa-thumbs-down"></i></span></div>
+                    <?php if ($answer['best'] == 1): ?>
+                        <div class="col-1"><span class="answer-winner"><i class="ikonka ikonka-winner fa-solid fa-circle-check"></i></span></span></div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <div class="divider mb20"></div>
--- views/questions/modules/non_user_question.php
+++ views/questions/modules/non_user_question.php
@@ -9,6 +9,8 @@
         <div class="col-12 question-summary-box">
             <span class="entry"><span class="light-txt">Created:</span> 2024-05-10</span>
             <span class="entry"><span class="light-txt">Modified:</span> 2024-05-10</span>
+            <span class="entry"><span class="light-txt">Best answer selected:</span> <?php echo  $hasBestAnswerSelected  ? '<i class="ikonka ikonka-winner fa-solid fa-circle-check"></i>'
+                    : '<i class="fa-solid fa-circle-xmark"></i>'; ?></span>
         </div>
     </div>
 </div>

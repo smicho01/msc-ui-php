0a2a66798f70b0d2b50fdb710c90f968cdd171a2
Seweryn <smicho01@student.bbk.ac.uk>
2024-07-08 19:36:54 +0100

    List user transactions

--- views/user/transactions.php
+++ views/user/transactions.php
@@ -15,4 +15,33 @@
         <h3 class="heading">Your Transactions</h3>
     </div>
 
+    <?php if(count($userTransactions) == 0) : ?>
+        <p>No transactions</p>
+    <?php else: ?>
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">Value</th>
+            <th scope="col">In/Out</th>
+            <th scope="col">Event Type</th>
+            <th scope="col">Transaction ID</th>
+            <th scope="col">Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($userTransactions as $transaction): ?>
+            <tr>
+                <td><?php echo $transaction['amount']; ?></td>
+                <td><?php echo ucwords($transaction['type']); ?></td>
+                <td><?php echo $transaction['event']; ?></td>
+                <td ><?php echo $transaction['id']; ?></td>
+                <td ><?php echo $transaction['date']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+
+        </tbody>
+    </table>
+    <?php endif; ?>
+
 </div>
